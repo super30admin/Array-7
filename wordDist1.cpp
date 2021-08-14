71822ee1a4f2e6e95a8c8c266326be07f1bb2865
Shreyas Nagendrarao <shreyasnagendrarao@Shreyass-MBP.lan>
2021-08-14 19:26:57 -0400

    Arrays-7 Complete

---
+++ wordDist1.cpp
@@ -0,0 +1,23 @@
+//Time:O((n) where n is the size of the wordsDict
+//Space:O(1) No Extra Space
+class Solution {
+public:
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        int p1 = -1;
+        int p2 = -1;
+        int shortDist = INT_MAX;
+        for(int i = 0; i < wordsDict.size();i++){
+            string word = wordsDict[i];
+            if(word == word1){
+                p1 = i;
+            }
+            if(word == word2){
+                p2 = i;
+            }
+            if(p1 != -1 and p2 != -1){
+               shortDist = min(shortDist, abs(p2-p1));
+            }
+        }
+        return shortDist;
+    }
+};
