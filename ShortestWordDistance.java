949007901967286d5d492167e0ac1275ac70415c
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-11-08 09:39:23 -0800

    Shortest Word Distance

---
+++ ShortestWordDistance.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Time Complexity : O(n) where n = length of wordsDict array
+//Space Complexity : O(1)
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+//243. Shortest Word Distance (Easy) - https://leetcode.com/problems/shortest-word-distance/description/
+//Time Complexity : O(n) where n = length of wordsDict array
+//Space Complexity : O(1)
+class Solution {
+	public int shortestDistance(String[] wordsDict, String word1, String word2) {
+		if (wordsDict == null || wordsDict.length == 0)
+			return 0;
+
+		int i1 = -1, i2 = -1, min = wordsDict.length;
+
+		for (int i = 0; i < wordsDict.length; i++) { // O(n)
+			String word = wordsDict[i];
+
+			if (word.equals(word1))
+				i1 = i;
+			if (word.equals(word2))
+				i2 = i;
+
+			if (i1 != -1 && i2 != -1) {
+				min = Math.min(min, Math.abs(i1 - i2));
+			}
+		}
+
+		return min;
+	}
+}
